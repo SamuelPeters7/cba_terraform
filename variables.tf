1c6661631af5a38771fdfa509d47dc81336c47f4
SamuelPeters7 <sam026777@gmail.com>
2023-10-14 14:34:58 +0100

    committing the changes

--- variables.tf
+++ variables.tf
@@ -6,16 +6,7 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "instance_ami" {
-  default = "ami-030594f92006445fd"
-}
-
-
-variable "vpc_id" {
-  default = ""
-}
-
-
 variable "key_name" {
   default = "cba_keypair"
 }
+
